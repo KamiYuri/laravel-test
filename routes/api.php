3ff94e18c71b02bc28707add617889077252d146
Hoang Viet Dung <vietdunghoang.hust@gmail.com>
2022-10-30 11:40:26 +0700

    asdsad

--- routes/api.php
+++ routes/api.php
@@ -23,5 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/testss', [TestController::class, "index"]);
 Route::get('test/abc', [TestController::class, 'abc']);
 
-Route::apiResource('test', TestController::class);
+Route::apiResource('tesdaasst', TestController::class);
 Route::apiResource('test5', Test5Controller::class);
